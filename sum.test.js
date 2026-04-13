e3465d0b244f5fe548b0c8d0d4cb9de765a6d6f5
Kaustubh-790 <kaustubhsharma434@gmail.com>
2026-04-13 11:41:52 +0530

    changes to sum fnx

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,11 @@
 import sum from "./sum.js";
 
-test("adds 2 + 2 equals to 4", () => {
-  expect(sum(2, 2)).toBe(4);
+describe("tests for sum fnx", () => {
+  test("adds 2 + 2 equals to 4", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+
+  test("adds -5 + -5 equals to -10", () => {
+    expect(sum(-5, -5)).toBe(-10);
+  });
 });
